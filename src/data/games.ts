5940cef4e857b6812ed3119757fd48c3ba016063
unknown <stewart.lhc@qq.com>
2025-03-31 21:13:29 +0800

    移动图片到 public 目录并更新游戏数据

--- src/data/games.ts
+++ src/data/games.ts
@@ -3,7 +3,7 @@ export const games = [
         "id": "1",
         "title": "Pixel Artist",
         "description": "",
-        "imageUrl": "/src/img/10001.png",
+        "imageUrl": "/img/10001.png",
         "category": "Art",
         "ageRange": "5-6",
         "src": "https://games.poki.com/458768/2fec5bbf-83a2-44b8-8fd8-a3e1923a325e?tag=kids"
@@ -12,7 +12,7 @@ export const games = [
         "id": "2",
         "title": "Happy Kittens Puzzle",
         "description": "",
-        "imageUrl": "/src/img/10002.png",
+        "imageUrl": "/img/10002.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/745bbb8a-518e-4fb6-bca2-f1cb1d062a0c?tag=kids"
@@ -21,7 +21,7 @@ export const games = [
         "id": "3",
         "title": "Penguin Cafe",
         "description": "",
-        "imageUrl": "/src/img/10003.png",
+        "imageUrl": "/img/10003.png",
         "category": "Art",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/531042f0-ac72-4551-9d8d-cc31059f40d1?tag=kids"
@@ -30,7 +30,7 @@ export const games = [
         "id": "4",
         "title": "Animal Connection",
         "description": "",
-        "imageUrl": "/src/img/10004.png",
+        "imageUrl": "/img/10004.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/b8f9bf25-f503-4fc3-9e57-1973e426e2cc?tag=kids"
@@ -39,7 +39,7 @@ export const games = [
         "id": "5",
         "title": "Snakes N Ladders",
         "description": "",
-        "imageUrl": "/src/img/10005.png",
+        "imageUrl": "/img/10005.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/028889ac-99b7-4170-addc-76c749f698a9?tag=kids"
@@ -48,7 +48,7 @@ export const games = [
         "id": "6",
         "title": "Pixel Skate",
         "description": "",
-        "imageUrl": "/src/img/10006.png",
+        "imageUrl": "/img/10006.png",
         "category": "Action",
         "ageRange": "7-8",
         "src": "https://games.poki.com/458768/309b01d3-aa3c-4afc-8c56-07b80c1da702?tag=kids"
@@ -57,7 +57,7 @@ export const games = [
         "id": "7",
         "title": "BeeLine",
         "description": "",
-        "imageUrl": "/src/img/10007.png",
+        "imageUrl": "/img/10007.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/5f092ae6-33a0-4b72-9523-58cec60ca663?tag=kids"
@@ -66,7 +66,7 @@ export const games = [
         "id": "08",
         "title": "Draw Parking",
         "description": "",
-        "imageUrl": "/src/img/10008.png",
+        "imageUrl": "/img/10008.png",
         "category": "Puzzle",
         "ageRange": "3 - 4",
         "src": "https://games.poki.com/458768/29ab5d67-e5be-4bca-9618-3781ef481c94?tag=kids"
@@ -75,7 +75,7 @@ export const games = [
         "id": "09",
         "title": "Draw Racing",
         "description": "",
-        "imageUrl": "/src/img/10009.png",
+        "imageUrl": "/img/10009.png",
         "category": "Action",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/c333cb7e-3ff9-4885-9678-c3ef6367ad42?tag=kids"
@@ -84,7 +84,7 @@ export const games = [
         "id": "10",
         "title": "Soccer Balls",
         "description": "",
-        "imageUrl": "/src/img/10010.png",
+        "imageUrl": "/img/10010.png",
         "category": "Action",
         "ageRange": "9-10",
         "src": "https://games.poki.com/458768/98dc06c5-160e-4f61-8f64-7267a5e69006?tag=kids"
@@ -93,7 +93,7 @@ export const games = [
         "id": "11",
         "title": "Happy Fishing",
         "description": "",
-        "imageUrl": "/src/img/10011.png",
+        "imageUrl": "/img/10011.png",
         "category": "Action",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/2262dd84-1d25-4f15-9626-1b094e90c9f2?tag=kids"
@@ -102,7 +102,7 @@ export const games = [
         "id": "12",
         "title": "Crashy Cat",
         "description": "",
-        "imageUrl": "/src/img/10012.png",
+        "imageUrl": "/img/10012.png",
         "category": "Action",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/60783d39-1df9-48ed-8a00-8fac7957bf75?tag=kids"
@@ -111,7 +111,7 @@ export const games = [
         "id": "13",
         "title": "Cosmic Bee",
         "description": "",
-        "imageUrl": "/src/img/10013.png",
+        "imageUrl": "/img/10013.png",
         "category": "Action",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/71576584-14d1-4520-96a9-1d6e3a081d0c?tag=kids"
@@ -120,7 +120,7 @@ export const games = [
         "id": "14",
         "title": "Paint Blue",
         "description": "",
-        "imageUrl": "/src/img/10014.png",
+        "imageUrl": "/img/10014.png",
         "category": "Art",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/4b452332-8ee3-4b80-b8a7-42f402db58e4?tag=kids"
@@ -129,7 +129,7 @@ export const games = [
         "id": "15",
         "title": "Bricks Master",
         "description": "",
-        "imageUrl": "/src/img/10015.png",
+        "imageUrl": "/img/10015.png",
         "category": "Puzzle",
         "ageRange": "9-10",
         "src": "https://games.poki.com/458768/6393f84e-d234-4dfb-a586-c52e6216fc67?tag=kids"
@@ -138,7 +138,7 @@ export const games = [
         "id": "16",
         "title": "Happy Crayons",
         "description": "",
-        "imageUrl": "/src/img/10016.png",
+        "imageUrl": "/img/10016.png",
         "category": "Art",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/4fce8d91-2064-4aa3-ba3f-235f1c2134e3?tag=kids"
@@ -147,7 +147,7 @@ export const games = [
         "id": "17",
         "title": "Mango Piggy Piggy Farm Harvest",
         "description": "",
-        "imageUrl": "/src/img/10017.png",
+        "imageUrl": "/img/10017.png",
         "category": "Action",
         "ageRange": "5-6",
         "src": "https://games.poki.com/458768/d095d293-ce78-4d4f-9b4d-d7c941adcb49?tag=kids"
@@ -156,7 +156,7 @@ export const games = [
         "id": "18",
         "title": "Happy Slushie",
         "description": "",
-        "imageUrl": "/src/img/10018.png",
+        "imageUrl": "/img/10018.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/21c81514-f8d0-4ddc-b930-da61445c1517?tag=kids"
@@ -165,7 +165,7 @@ export const games = [
         "id": "19",
         "title": "My Pet Clinic",
         "description": "",
-        "imageUrl": "/src/img/10019.png",
+        "imageUrl": "/img/10019.png",
         "category": "Art",
         "ageRange": "5-6",
         "src": "https://games.poki.com/458768/fd4ea956-fe9f-4c77-b6cc-d46670604f43?tag=kids"
@@ -174,7 +174,7 @@ export const games = [
         "id": "20",
         "title": "Ollie Goes to School",
         "description": "",
-        "imageUrl": "/src/img/10020.png",
+        "imageUrl": "/img/10020.png",
         "category": "RPG",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/3f867904-a8e8-4083-9b56-e59deb012464?tag=kids"
@@ -183,7 +183,7 @@ export const games = [
         "id": "21",
         "title": "Unicorns Birthday Surprise",
         "description": "",
-        "imageUrl": "/src/img/10021.png",
+        "imageUrl": "/img/10021.png",
         "category": "Art",
         "ageRange": "7-8",
         "src": "https://games.poki.com/458768/5ba4c031-a66a-43f4-9595-7839806319e1?tag=kids"
@@ -192,7 +192,7 @@ export const games = [
         "id": "22",
         "title": "Funny Rescue Carpenter",
         "description": "",
-        "imageUrl": "/src/img/10022.png",
+        "imageUrl": "/img/10022.png",
         "category": "Action",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/0f2d131c-0b08-4273-8a17-666aa7b91e32?tag=kids"
@@ -201,7 +201,7 @@ export const games = [
         "id": "23",
         "title": "Funny Rescue Gardener",
         "description": "",
-        "imageUrl": "/src/img/10023.png",
+        "imageUrl": "/img/10023.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/e54cb6a7-e9e9-4bb8-b600-06b21381dc83?tag=kids"
@@ -210,7 +210,7 @@ export const games = [
         "id": "24",
         "title": "Funny Pet Rescue",
         "description": "",
-        "imageUrl": "/src/img/10024.png",
+        "imageUrl": "/img/10024.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/0576b48c-1221-40dc-8a9c-10c7b7543a7a?tag=kids"
@@ -219,7 +219,7 @@ export const games = [
         "id": "25",
         "title": "Funny Rescue Zookeeper",
         "description": "",
-        "imageUrl": "/src/img/10025.png",
+        "imageUrl": "/img/10025.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/b4ada3bc-edf7-444e-8e5f-813b543526f0?tag=kids"
@@ -228,7 +228,7 @@ export const games = [
         "id": "26",
         "title": "Baby Race Galaxy",
         "description": "",
-        "imageUrl": "/src/img/10026.png",
+        "imageUrl": "/img/10026.png",
         "category": "Action",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/4186bbd6-23fe-402a-8e17-0179ba8f2e69?tag=kids"
@@ -237,7 +237,7 @@ export const games = [
         "id": "27",
         "title": "Chu Choo Cake",
         "description": "",
-        "imageUrl": "/src/img/10027.png",
+        "imageUrl": "/img/10027.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/c647423f-5cb9-42f3-8290-e5bba47340e5?tag=kids"
@@ -246,7 +246,7 @@ export const games = [
         "id": "28",
         "title": "Creative Puzzle",
         "description": "",
-        "imageUrl": "/src/img/10028.png",
+        "imageUrl": "/img/10028.png",
         "category": "Puzzle",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/4f838078-198f-401d-b89f-7a9ec79990f7?tag=kids"
@@ -255,7 +255,7 @@ export const games = [
         "id": "29",
         "title": "Sweety Memory",
         "description": "",
-        "imageUrl": "/src/img/10029.png",
+        "imageUrl": "/img/10029.png",
         "category": "Art",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/bd45a718-4eb5-4dfa-80c7-78747deac999?tag=kids"
@@ -264,7 +264,7 @@ export const games = [
         "id": "30",
         "title": "Coloring Book",
         "description": "",
-        "imageUrl": "/src/img/10030.png",
+        "imageUrl": "/img/10030.png",
         "category": "Art",
         "ageRange": "3-4",
         "src": "https://games.poki.com/458768/f2a150b4-02a1-468a-aca8-ab7b2610478f?tag=kids"
